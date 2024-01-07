0012c7f375fc80a3e0269fb94c49f5241f7bc599
Shuklashashi0706 <shuklashashi169@gmail.com>
2024-01-08 02:03:44 +0530

    Updated

--- Frontend/src/screens/ProfileScreen.jsx
+++ Frontend/src/screens/ProfileScreen.jsx
@@ -8,88 +8,90 @@ import Loader from "../components/Loader";
 import { useUpdateProfileMutation } from "../slices/userApiSlice";
 import { setCredentials } from "../slices/authSlice";
 const ProfileScreen = () => {
-  console.log("Profile Screen");
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   const dispatch = useDispatch();
-  const userInfo = useSelector((state) => {
-    state.auth;
-  });
+
+  const { userInfo } = useSelector((state) => state.auth);
+
   const [updateProfile, { isLoading }] = useUpdateProfileMutation();
+
   useEffect(() => {
     setName(userInfo.name);
     setEmail(userInfo.email);
   }, [userInfo.email, userInfo.name]);
+
   const submitHandler = async (e) => {
     e.preventDefault();
-    e.preventDefault();
     if (password !== confirmPassword) {
-      toast.error("Passwords do not match ");
+      toast.error('Passwords do not match');
     } else {
       try {
         const res = await updateProfile({
           _id: userInfo._id,
-          name: name,
-          email: email,
-          password: password,
+          name,
+          email,
+          password,
         }).unwrap();
-        dispatch(setCredentials({ ...res }));
-        toast.success("Profile updated");
-      } catch (error) {
-        toast.error(error?.data?.data?.message || error.error);
+        console.log(res);
+        dispatch(setCredentials(res));
+        toast.success('Profile updated successfully');
+      } catch (err) {
+        toast.error(err?.data?.message || err.error);
       }
     }
   };
-
   return (
     <FormContainer>
       <h1>Update Profile</h1>
 
       <Form onSubmit={submitHandler}>
-        <Form.Group className="my-2" controlId="name">
+        <Form.Group className='my-2' controlId='name'>
           <Form.Label>Name</Form.Label>
           <Form.Control
-            type="name"
-            placeholder="Enter name"
+            type='name'
+            placeholder='Enter name'
             value={name}
             onChange={(e) => setName(e.target.value)}
           ></Form.Control>
         </Form.Group>
-        <Form.Group className="my-2" controlId="email">
+        <Form.Group className='my-2' controlId='email'>
           <Form.Label>Email Address</Form.Label>
           <Form.Control
-            type="email"
-            placeholder="Enter email"
+            type='email'
+            placeholder='Enter email'
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           ></Form.Control>
         </Form.Group>
-        <Form.Group className="my-2" controlId="password">
+        <Form.Group className='my-2' controlId='password'>
           <Form.Label>Password</Form.Label>
           <Form.Control
-            type="password"
-            placeholder="Enter password"
+            type='password'
+            placeholder='Enter password'
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
-        <Form.Group className="my-2" controlId="confirmPassword">
+        <Form.Group className='my-2' controlId='confirmPassword'>
           <Form.Label>Confirm Password</Form.Label>
           <Form.Control
-            type="password"
-            placeholder="Confirm password"
+            type='password'
+            placeholder='Confirm password'
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
-        {isLoading && <Loader />}
-        <Button type="submit" variant="primary" className="mt-3">
+
+        <Button type='submit' variant='primary' className='mt-3'>
           Update
         </Button>
+
+        {isLoading && <Loader />}
       </Form>
     </FormContainer>
   );
