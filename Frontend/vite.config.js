d73c36244c4c8b7b89edf481243d08c6b0b75386
Shuklashashi0706 <shuklashashi169@gmail.com>
2024-01-10 20:59:01 +0530

    Updated

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port:3000,
     proxy:{
       '/api':{
-        target:'http://localhost:5000',
+        target:'https://auth-3i6i.onrender.com',
         changeOrigin:true
       }
     }
